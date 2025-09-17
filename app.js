2024364204b77f3e82e23bacea908a6238a10c35
Rikael Ribeiro <rikaelribeiro52@gmail.com>
2025-09-17 13:11:32 -0300

    Update app.js v3

--- app.js
+++ app.js
@@ -35,33 +35,7 @@ app.post('/enviar', (req, res) => {
     //email
   //});
      res.send(`
-    <style>
-    body, html {
-      margin: 0;
-      padding: 0;
-      height: 100%;
-      background: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    .loader {
-      width: 60px;
-      height: 60px;
-      border: 6px solid #f3f3f3;
-      border-top: 6px solid #1a73e8;
-      border-radius: 50%;
-      animation: spin 1s linear infinite;
-    }
-    @keyframes spin {
-      0% { transform: rotate(0deg); }
-      100% { transform: rotate(360deg); }
-    }
-  </style>
-</head>
-<body>
-  <div class="loader"></div>
-</body>
+   <h1>Funcionando</h1>
   `);
 
 });
